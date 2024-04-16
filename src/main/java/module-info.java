2fd2748e5b9babc0b8076f4032339420fd38c2e8
Ankita <Ankitashejal11@gmail.com>
2024-04-16 12:19:04 +0530

    snake game

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+
+    opens com.example.demo3 to javafx.fxml;
+    exports com.example.demo3;
+}
